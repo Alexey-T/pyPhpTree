0df91c154570887a04723b7060de8095bf5c5c0b
Alexey-t <support@uvviewsoft.com>
2018-05-15 17:47:26 +0300

    init

---
+++ testfiles/a.php
@@ -0,0 +1,17 @@
+<?
+  function F1(a,b){
+  }
+  function /*t*/F2
+    /*t*/
+    (/*t*/)/*t*/
+    {
+    }
+?>
+
+<?
+  function F3(a){
+  }
+?>
+
+function Z(){} <? function F4(a){} ?>
+function Z2(){}
---
+++ testfiles/c.php
@@ -0,0 +1,24 @@
+<?
+  class C1 {
+    function F1a(a,b){
+      }
+    function /*t*/F1b
+      /*t*/
+      (/*t*/)/*t*/
+      {
+      }
+?>
+
+<?
+  } /*end of C1*/
+  
+  class C2 /*t*/{
+    function F2a(a){
+    }
+?>
+
+function Z(){} <?
+    function F2b(){} 
+    }
+  } /*end if C2*/
+?>
