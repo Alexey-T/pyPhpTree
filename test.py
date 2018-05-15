cad29276fa7475cc220399f1ef1cdd86e7cd014d
Alexey-t <support@uvviewsoft.com>
2018-05-15 21:47:56 +0300

    better test

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ fn2 = os.path.join(dir, 'testfiles', 'c.php')
 def test_file(fn):
     with open(fn, encoding='utf8') as f:
         lines = f.read().splitlines()
-        print('Headers in: '+fn)
-        print(list(pyphptree.get_headers(lines)))
+        print('\nHeaders in: '+fn)
+        for item in pyphptree.get_headers(lines):
+            print('  ', item)
 
 test_file(fn1)
 test_file(fn2)
