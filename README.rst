1a980552ebef1d6f8abe4150346585f40c7086e7
Artem3213212 <cx-1212@yandex.ru>
2018-06-14 19:05:54 +0300

    1.0.3

--- README.rst
+++ README.rst
@@ -27,9 +27,3 @@ Function ``get_headers(filename, lines)`` finds all entities, in given
    }
 
 It's generator (yield), so to get list, use ``list(get_headers(...))``.
-
-About
-=====
-
--  Author: Alexey Torgashin, UVviewsoft.com
--  License: Mozilla Public License 2.0 (MPL-2.0)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Alexey Torgashin',
     author_email='support@uvviewsoft.com',
     url='https://github.com/Alexey-T/pyPhpTree',
-    license='Mozilla Public License 2.0 (MPL 2.0)',
+    #license='Mozilla Public License 2.0 (MPL 2.0)',
     classifiers=(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
