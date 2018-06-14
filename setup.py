6d66c97581b31a3c23b2833c76ec2a1a16bd56bc
Artem3213212 <cx-1212@yandex.ru>
2018-06-14 18:29:18 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='pyPhpTree',
+    name='pyphptree',
     version='1.0.0',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alexey Torgashin',
     author_email='support@uvviewsoft.com',
     url='http://uvviewsoft.com/',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: PHP",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ),
 )
